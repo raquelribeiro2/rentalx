006c92337d26cdb131345b2b4c291213e38507bc
raquelribeiro2 <raqu3l.rodriguesribeiro@gmail.com>
2021-04-08 17:50:35 -0300

    Addition of administrator middleware

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,11 +1,12 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async (host = 'database'): Promise<Connection> => {
+export default async (host = 'database_ignite'): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
       host,
+      port: 5432,
     }),
   );
 };
