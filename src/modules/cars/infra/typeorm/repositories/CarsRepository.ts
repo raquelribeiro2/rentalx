24014a4e7aaa179d6ba79c38b7f54973e42ebd39
raquelribeiro2 <raqu3l.rodriguesribeiro@gmail.com>
2021-04-08 19:08:55 -0300

    List of available cars

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -41,6 +41,32 @@ class CarsRepository implements ICarsRepository {
 
     return car;
   }
+
+  async findAvailable(
+    category_id?: string,
+    brand?: string,
+    name?: string,
+  ): Promise<Car[]> {
+    const carsQuery = await this.repository
+      .createQueryBuilder('cars')
+      .where('available = :available', { available: true });
+
+    if (category_id) {
+      carsQuery.andWhere('category_id = :category_id', { category_id });
+    }
+
+    if (brand) {
+      carsQuery.andWhere('brand = :brand', { brand });
+    }
+
+    if (name) {
+      carsQuery.andWhere('name = :name', { name });
+    }
+
+    const cars = await carsQuery.getMany();
+
+    return cars;
+  }
 }
 
 export { CarsRepository };
